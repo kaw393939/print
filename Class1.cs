3a7ed4605b4e9f6078f80f8abc31f7529685269c
Keith Williams <keith@webizly.com>
2019-12-23 14:55:56 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace print
+{
+    public interface IPrinter
+    {
+        public void Print(String input);
+        public void Print(String[] input);
+    }
+    public class Printer : IPrinter
+    {
+        public void Print(String input) {
+            Utilities.Print(input);
+        }
+        public void Print(String[] input)
+        {
+            foreach (string line in input)
+            {
+                Utilities.Print(line);
+            }
+        }
+    }
+    public static class Utilities {
+        public static void Print(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
